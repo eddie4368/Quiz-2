02c5b68cf1fc8b1a44b424c2b7f499195793b5d2
eddie ortega <23edortega@gmail.com>
2024-08-28 12:56:27 -0500

    Add project files.

---
+++ Quiz 2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Services;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Schema;
+
+namespace Quiz_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string  Total;
+            Console.Write("Enter a demical number for a percentage such 0.05 for 5%: "); // user input fpr percent 
+            Total = Console.ReadLine(); // input 
+            Console.Write("enter the total amount"); // user input total amout 
+            Console.ReadLine(); // Display output
+
+            
+        }
+    }
+}
